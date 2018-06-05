67b1b1e73eac1fc573fc6c1854cabeec5fc81b4b
Jack Xu <jackexyou@gmail.com>
2018-06-05 14:55:04 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import React from 'react';  // import React from 'react' // We need to import react so we can make use of its .component class
+import ReactDOM from 'react-dom'; // import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
 
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
@@ -6,33 +8,54 @@ const BUTCHER_PRODUCTS = [
   'Ribeye'
 ]
 
+    const products = BUTCHER_PRODUCTS.map((p, idx) => (<li key={idx}>{p}</li>))
+
 // Define these exported classes
 export class OlderCoaster extends React.Component {
+
+  // this function should return the JSX we want the DOM to render
    render() {
      return (
-      <p>Two grannies having the time of their life!</p>
-      <p>Passengers:</p>
-      <ul>
-        <li>Agnes</li>
-        <li>Muriel</li>
-      </ul>
+  //     <div>Hello!</div>
+        <div class="oldercoaster">
+          <p>Two grannies having the time of their life!</p>
+          <p>Passengers:</p>
+            <ul>
+              <li>Agnes</li>
+              <li>Muriel</li>
+            </ul>
+        </div>
      )
    }
+  //
 }
 
 export class InFrontOfYou extends React.Component {
+  // your code here
   render() {
     return (
-      <div>
-        <p>You shouldn't look too far.</p>
-        <p>Sometimes, the solution is right in front of you.</p>
-      </div>  
+ //     <div>Hello!</div>
+ <div>
+ <p>You shouldn&#x27;t look too far.</p>
+ <p>Sometimes, the solution is right in front of you.</p>
+</div>
     )
   }
+ //
 }
 
 export class ButcherShop extends React.Component {
   // your code here
+  render() {
+    return (
+      <div className="butcher-shop">
+        <p>Hello! We have the following products for sale today:</p>
+          <ul>
+            {products}
+          </ul>
+      </div>
+    )
+  }
 }
 
 
@@ -40,7 +63,9 @@ export class App extends React.Component {
   render() {
     return (
       <div id="app">
-
+        <OlderCoaster />
+        <InFrontOfYou />
+        <ButcherShop />
       </div>
     )
   }
